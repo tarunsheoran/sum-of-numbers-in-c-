bb6dadb087da68b13d93b3212fb3b1e74345364f
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-07-31 21:20:34 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,sum;
+    cout<<"Enter the numbers\n";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"The sum is="<<sum<<"\n";
+    cout<<"The sum of "<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
